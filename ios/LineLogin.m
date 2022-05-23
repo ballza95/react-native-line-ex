9b1591cb20007f0913ae029832d05242eefc78c5
Piyapan Ball <>
2022-05-23 16:09:52 +0700

    fix ios #8

--- ios/LineLogin.m
+++ ios/LineLogin.m
@@ -32,30 +32,3 @@
   BOOL handledLine = [LineLogin application:application continue:userActivity restorationHandler:restorationHandler];
   return handledLine;
 }
-
-@interface RCT_EXTERN_MODULE(LineLogin, NSObject)
-  RCT_EXTERN_METHOD(login: (NSDictionary *)arguments
-                  resolver: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject
-                  )
-  RCT_EXTERN_METHOD(logout: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject)
-  RCT_EXTERN_METHOD(
-                  getBotFriendshipStatus: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject
-                  )
-  RCT_EXTERN_METHOD(getCurrentAccessToken: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject)
-  RCT_EXTERN_METHOD(
-                  getProfile: (RCTPromiseResolveBlock)resolve
-                        rejecter: (RCTPromiseRejectBlock)reject
-                  )
-  RCT_EXTERN_METHOD(
-                  refreshToken: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject
-                  )
-  RCT_EXTERN_METHOD(
-                  verifyAccessToken: (RCTPromiseResolveBlock)resolve
-                  rejecter: (RCTPromiseRejectBlock)reject
-                  )
-@end
